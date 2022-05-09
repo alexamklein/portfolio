4986b30072286cfa4c432352caacc9ae73c1d3ba
alexamklein <99451706+alexamklein@users.noreply.github.com>
2022-05-09 02:37:39 -0400

    Added a favicon and a navbar logo + fixed an issue with the navbar + activated the social media links + added an about section

--- src/script.js
+++ src/script.js
@@ -1,3 +1,64 @@
+function displayEmail(event) {
+  event;
+  document.querySelector("#email").innerHTML = " alexa.m.klein@gmail.com";
+}
+
+function hideEmail(event) {
+  event;
+  document.querySelector("#email").innerHTML = "";
+}
+
+function displayGithub(event) {
+  event;
+  document.querySelector("#github").innerHTML = " github.com/alexamklein";
+}
+
+function hideGithub(event) {
+  event;
+  document.querySelector("#github").innerHTML = "";
+}
+
+function displayLinkedin(event) {
+  event;
+  document.querySelector("#linkedin").innerHTML =
+    " linkedin.com/in/alexa-m-klein";
+}
+
+function hideLinkedin(event) {
+  event;
+  document.querySelector("#linkedin").innerHTML = "";
+}
+
+function displayInstagram(event) {
+  event;
+  document.querySelector("#instagram").innerHTML = " @alexamklein";
+}
+
+function hideInstagram(event) {
+  event;
+  document.querySelector("#instagram").innerHTML = "";
+}
+
+function displayTwitter(event) {
+  event;
+  document.querySelector("#twitter").innerHTML = " @alexamklein";
+}
+
+function hideTwitter(event) {
+  event;
+  document.querySelector("#twitter").innerHTML = "";
+}
+
+function displayCodepen(event) {
+  event;
+  document.querySelector("#codepen").innerHTML = " @alexamklein";
+}
+
+function hideCodepen(event) {
+  event;
+  document.querySelector("#codepen").innerHTML = "";
+}
+
 function playAnim() {
   setTimeout(
     function () {
@@ -28,6 +89,30 @@ function playAnim() {
     isAdding ? 120 : 60
   );
 }
+
+let emailLink = document.querySelector("#email-icon");
+emailLink.addEventListener("mouseover", displayEmail);
+emailLink.addEventListener("mouseout", hideEmail);
+
+let githubLink = document.querySelector("#github-icon");
+githubLink.addEventListener("mouseover", displayGithub);
+githubLink.addEventListener("mouseout", hideGithub);
+
+let linkedinLink = document.querySelector("#linkedin-icon");
+linkedinLink.addEventListener("mouseover", displayLinkedin);
+linkedinLink.addEventListener("mouseout", hideLinkedin);
+
+let instagramLink = document.querySelector("#instagram-icon");
+instagramLink.addEventListener("mouseover", displayInstagram);
+instagramLink.addEventListener("mouseout", hideInstagram);
+
+let twitterLink = document.querySelector("#twitter-icon");
+twitterLink.addEventListener("mouseover", displayTwitter);
+twitterLink.addEventListener("mouseout", hideTwitter);
+
+let codepenLink = document.querySelector("#codepen-icon");
+codepenLink.addEventListener("mouseover", displayCodepen);
+codepenLink.addEventListener("mouseout", hideCodepen);
 
 let typeText = document.querySelector(".typeText");
 let textToBeTyped = "front-end developer.";
